b5e1abc5146684d0ddfe56b6766c1f83159cf59a
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-09-10 08:44:39 +0200

    Add Loader and Error components to the Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Home from './ui/Home'
-import Menu from './features/menu/Menu'
+import Error from './ui/Error'
+import AppLayout from './ui/AppLayout'
+
+import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
-import AppLayout from './ui/AppLayout'
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -18,6 +21,8 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: '/cart',
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Outlet, useNavigation } from 'react-router-dom'
+
 import Header from './Header'
 import CartOverview from '../features/cart/CartOverview'
-import { Outlet } from 'react-router-dom'
+import Loader from '../ui/Loader'
 
 export default function AppLayout() {
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
