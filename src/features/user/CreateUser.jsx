57ac41d481bfb950c669ae8bd958636b9c5481f6
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-09-10 21:37:49 +0200

    Add some styles with tailwind

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState('')
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
       {username !== '' && (
@@ -24,7 +27,7 @@ function CreateUser() {
         </div>
       )}
     </form>
-  );
+  )
 }
 
-export default CreateUser;
+export default CreateUser
