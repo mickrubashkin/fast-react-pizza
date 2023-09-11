e7dde147dcdff920e31b728264160d42110d8748
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-09-11 19:35:48 +0200

    Replace dummy cart data in the Cart. Add getCart selector to the cart slice

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -5,7 +5,8 @@ import { getTotalCartPrice, getTotalCartQuantity } from './cartSlice'
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity)
   const totalCartPrice = useSelector(getTotalCartPrice)
-  console.log(totalCartQuantity)
+
+  if (!totalCartQuantity) return null
 
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
